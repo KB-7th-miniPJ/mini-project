2588a3ab67d93bac5d162813f883da04b5f318f5
guhs1 <ktw5214.10@gmail.com>
2026-04-10 16:16:05 +0900

    Update ExpenseMembers.vue

--- src/pages/ExpenseMembers.vue
+++ src/pages/ExpenseMembers.vue
@@ -127,7 +127,7 @@ const handleComplete = () => {
     selectedParticipants.value.length,
   );
 
-  router.back(/travels/:travelId/expenses/new);
+  router.back("/travels/:travelId/expenses/new");
 };
 
 onMounted(async () => {
@@ -141,7 +141,7 @@ onMounted(async () => {
 });
 
 const goBack = () => {
-  router.back(/travels/:travelId/expenses/new);
+  router.back("/travels/:travelId/expenses/new");
 };
 </script>
 
