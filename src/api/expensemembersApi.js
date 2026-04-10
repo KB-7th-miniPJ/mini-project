edd14e45ddea00a2d2373ff5bb511d8abf195269
KKFKFKdd <ktw5214.10@gmail.com>
2026-04-10 09:50:16 +0900

    fix:멤버목록오류수정

--- src/api/expensemembersApi.js
+++ src/api/expensemembersApi.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const BASE_URL = 'http://localhost:3000'
 
-// 전체 유저 가져온 뒤 travelId로 필터링
+// Exp-06 전체 유저 가져온 뒤 travelId로 필터링
 export const getMembersByTravelId = async (travelId) => { 
   const res = await axios.get(`${BASE_URL}/users`) 
   return res.data.filter(user => {
